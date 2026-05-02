195438e3cd0e48b2171b1db3121f35b56080ac4b
AKTags CI <ci@akinus21.com>
2026-05-02 14:09:32 +0000

    chore: update aktags to v0.0.5

--- Formula/aktags.rb
+++ Formula/aktags.rb
@@ -1,11 +1,11 @@
 class Aktags < Formula
   desc "AI-powered tag-based file browser with background daemon"
   homepage "https://github.com/Akinus21/Aktags"
-  version "0.0.4"
+  version "0.0.5"
 
   on_linux do
-    url "https://github.com/Akinus21/Aktags/releases/download/v0.0.4/aktags"
-    sha256 "68412824e77d41a977517754a14bc7d00c6047ff115e60d81bb6755f378512a8"
+    url "https://github.com/Akinus21/Aktags/releases/download/v0.0.5/aktags"
+    sha256 "f75729747e8e7eb5116d9ff0a4616603dfa83d61b6ab7170af3e59cc28e20330"
   end
 
   def install
